34d5aece62a8b11e82692bb42eb2b98e16595b18
maddrum <maddrum9@gmail.com>
2018-06-14 10:36:52 +0300

    Deployment site some redesigns

--- World_Cup_results/settings.py
+++ World_Cup_results/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 with open('key_no_git_upload.txt') as f:
     SECRET_KEY = f.read().strip()
-print(SECRET_KEY)
+
 
 # Developing settings - activate this set for developing
 
