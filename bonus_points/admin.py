17ffa9f2b8c09c542065c787eb8f3bf0b9e25fd6
maddrum <maddrum9@gmail.com>
2018-06-16 13:13:31 +0300

    Deployment site Bonus Points Initial

--- bonus_points/admin.py
+++ bonus_points/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from bonus_points.models import BonusDescription, BonusUserPrediction, UserBonusSummary
 
 # Register your models here.
+admin.site.register(BonusDescription)
+admin.site.register(BonusUserPrediction)
+admin.site.register(UserBonusSummary)
