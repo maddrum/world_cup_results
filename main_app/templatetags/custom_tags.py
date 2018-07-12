0a46da9307cf4a14357aed5aeec2707f1b6f0af5
maddrum <maddrum9@gmail.com>
2018-07-12 10:03:15 +0300

    1.3.12 - bug fixes in blinker bonus points

--- main_app/templatetags/custom_tags.py
+++ main_app/templatetags/custom_tags.py
@@ -8,7 +8,8 @@ register = template.Library()
 @register.simple_tag
 def active_bonuses_checker():
     current_time = datetime.datetime.now()
-    all_bonuses = BonusDescription.objects.filter(active_until__gte=current_time, archived=False).count()
+    all_bonuses = BonusDescription.objects.filter(active_until__gte=current_time, archived=False,
+                                                  bonus_active=True).count()
     if all_bonuses != 0:
         return True
     else:
